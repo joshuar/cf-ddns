d16a3ffac7158111574f8b78d63f26ed26ea063e
Joshua Rich <joshua.rich@gmail.com>
2022-12-11 10:39:57 +1000

    Fix parsing of configuration file argument. Add a postinstall script.

--- cmd/root.go
+++ cmd/root.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -81,9 +81,9 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "c", "config file (default is $HOME/.cf-ddns.yaml)")
-	rootCmd.Flags().BoolVarP(&debugFlag, "debug", "d", false, "debug output")
-	rootCmd.Flags().BoolVarP(&profileFlag, "profile", "p", false, "enable profiling")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "/etc/cf-ddns/cf-ddns.yml", "config file (default is /etc/cf-ddns/cf-ddns.yaml)")
+	rootCmd.Flags().BoolVarP(&debugFlag, "debug", "d", false, "debug output (default is false)")
+	rootCmd.Flags().BoolVarP(&profileFlag, "profile", "p", false, "enable profiling (default is false)")
 }
 
 // initConfig reads in config file and ENV variables if set.
